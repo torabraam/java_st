62460f44f17fa70e2873332d97a24a8b136919f3
torabraam <solodovnikova.victoria@gmail.com>
2021-07-14 15:38:20 +0300

    corrections for test

--- sandbox/src/test/java/ru/st/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/st/sandbox/PointTest.java
@@ -7,17 +7,17 @@ public class PointTest {
     @Test
     // simple positive
     public void distance2Test1() {
-        Point p1 = new Point(15.0, 3.0);
-        Point p2 = new Point(5.0, 3.0);
-        assert p1.distance2(p2) == 10;
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(0, 0);
+        assert p1.distance2(p2) == 0;
     }
 
     @Test
     // with Assert class positive
     public void distance2Test2() {
-        Point p1 = new Point(15.0, 3.0);
+        Point p1 = new Point(-1.0, 3.0);
         Point p2 = new Point(5.0, 3.0);
-        Assert.assertEquals(p1.distance2(p2), 10.0);
+        Assert.assertEquals(p1.distance2(p2), 6.0);
     }
 
     @Test
