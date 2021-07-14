6f90c68ed519509266527cb947095ae8a2c8d320
torabraam <solodovnikova.victoria@gmail.com>
2021-07-14 15:33:24 +0300

    create 1st test

---
+++ sandbox/src/test/java/ru/st/sandbox/PointTest.java
@@ -0,0 +1,30 @@
+package ru.st.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+    @Test
+    // simple positive
+    public void distance2Test1() {
+        Point p1 = new Point(15.0, 3.0);
+        Point p2 = new Point(5.0, 3.0);
+        assert p1.distance2(p2) == 10;
+    }
+
+    @Test
+    // with Assert class positive
+    public void distance2Test2() {
+        Point p1 = new Point(15.0, 3.0);
+        Point p2 = new Point(5.0, 3.0);
+        Assert.assertEquals(p1.distance2(p2), 10.0);
+    }
+
+    @Test
+    // with Assert class negative
+    public void distance2Test3() {
+        Point p1 = new Point(15.0, 3.0);
+        Point p2 = new Point(5.0, 3.0);
+        Assert.assertEquals(p1.distance2(p2), 15.0);
+    }
+}
