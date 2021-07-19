1ee166cb859f8e5c6f483fb69b4b9a90e793e32e
torabraam <solodovnikova.victoria@gmail.com>
2021-07-19 23:46:41 +0300

    add TestBase class and refactor GroupCreation, GroupDeletion

---
+++ addressbook-web-tests/src/test/java/ru/st/addressbook/GroupDeletionTests.java
@@ -0,0 +1,17 @@
+package ru.st.addressbook;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+    @Test
+    public void testGroupDeletion() throws Exception {
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+
+}
