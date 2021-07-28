de8d1419ee3faab232300f8355fdc360a612e7aa
torabraam <solodovnikova.victoria@gmail.com>
2021-07-28 17:23:10 +0300

    check group exists before modify/delete

--- addressbook-web-tests/src/test/java/ru/st/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/st/addressbook/tests/GroupModificationTests.java
@@ -3,11 +3,14 @@ package ru.st.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.st.addressbook.model.GroupData;
 
-public class GroupModificationTests extends TestBase{
+public class GroupModificationTests extends TestBase {
 
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
