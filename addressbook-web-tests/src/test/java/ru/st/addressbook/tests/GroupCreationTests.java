e31c8ac4a4158ce0a53fce1ea91949e388c62e22
torabraam <solodovnikova.victoria@gmail.com>
2021-08-04 22:21:43 +0300

    add size check 4.3

--- addressbook-web-tests/src/test/java/ru/st/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/st/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.st.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.st.addressbook.model.GroupData;
 
@@ -10,8 +11,10 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
 
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
     }
 
 }
