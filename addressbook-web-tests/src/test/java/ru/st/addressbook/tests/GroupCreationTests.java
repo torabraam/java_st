cfd6cd5fa9e8026a9e60a208d70d1f9d831f0031
torabraam <solodovnikova.victoria@gmail.com>
2021-08-04 22:52:01 +0300

    add comparing size of list 4.5

--- addressbook-web-tests/src/test/java/ru/st/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/st/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,7 @@ package ru.st.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.st.addressbook.model.GroupData;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
@@ -11,10 +12,10 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
 
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before + 1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
     }
 
 }
