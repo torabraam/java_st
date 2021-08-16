a4c1a121842e9e5e5237447dadf81f3b215cdb28
torabraam <solodovnikova.victoria@gmail.com>
2021-08-17 00:00:32 +0300

    add try to dataProvider

--- addressbook-web-tests/src/test/java/ru/st/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/st/addressbook/tests/ContactCreationTests.java
@@ -22,20 +22,21 @@ public class ContactCreationTests extends TestBase {
 
     @DataProvider
     public Iterator<Object[]> validContactsFromJSON() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
-        String json = "";
-        String line = reader.readLine();
-        while (line != null) {
-            json += line;
-            line = reader.readLine();
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))) {
+            String json = "";
+            String line = reader.readLine();
+            while (line != null) {
+                json += line;
+                line = reader.readLine();
+            }
+            Gson gson = new Gson();
+            List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+            }.getType()); //List<ContactData>.class
+            return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
         }
-        Gson gson = new Gson();
-        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
-        }.getType()); //List<ContactData>.class
-        return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
 
-    @Test (dataProvider = "validContactsFromJSON")
+    @Test(dataProvider = "validContactsFromJSON")
     public void testContactCreationTests(ContactData contact) { //before was void testContactCreationTests()
 
         File photo = new File("src/test/resources/pic1.png");
@@ -53,7 +54,7 @@ public class ContactCreationTests extends TestBase {
                 before.withAdded(contact.withId(after.stream().mapToInt((u) -> u.getId()).max().getAsInt()))));
     }
 
-    @Test (enabled = false)
+    @Test(enabled = false)
     public void testBadContactCreationTests() {
 
         Contacts before = app.contact().allc();
